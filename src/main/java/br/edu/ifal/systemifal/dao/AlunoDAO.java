8266d24f3417717fe08b99d5aa1df92d23da44d2
Ana clara <anna.claara98@gmail.com>
2018-02-02 15:53:29 -0300

    Criação do DAO para Aluno

---
+++ src/main/java/br/edu/ifal/systemifal/dao/AlunoDAO.java
@@ -0,0 +1,40 @@
+package br.edu.ifal.systemifal.dao;
+
+import java.util.List;
+
+import br.edu.ifal.systemifal.modelo.Aluno;
+
+public class AlunoDAO implements DAOInterface<Aluno, String>{
+
+	public void salvar(Aluno entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void atualizar(Aluno entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Aluno buscarPorId(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deletar(Aluno entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Aluno> buscarTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deletarTodos() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ src/main/java/br/edu/ifal/systemifal/dao/DAOInterface.java
@@ -0,0 +1,19 @@
+package br.edu.ifal.systemifal.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+
+public interface DAOInterface<T, Id extends Serializable> {
+	
+	public void salvar(T entity);
+	public void atualizar(T entity);
+	public  T buscarPorId(Id id);
+	public void deletar(T entity);
+	public List<T> buscarTodos();
+	public void deletarTodos();
+	
+	
+
+}
