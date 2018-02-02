ef75f19e9237b5513ad0e8bd7255a7d31c18708b
Ana clara <anna.claara98@gmail.com>
2018-02-02 15:29:13 -0300

    Inicio do projeto usando maven

---
+++ src/main/java/br/edu/ifal/systemifal/modelo/Aluno.java
@@ -0,0 +1,74 @@
+package br.edu.ifal.systemifal.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="aluno")
+public class Aluno {
+	
+	@Id
+	private String id;
+	
+	@Column
+	private String matricula;
+	
+	@Column
+	private String nome;
+	
+	
+	public Aluno(String id, String matricula, String nome) {
+		super();
+		this.id = id;
+		this.matricula = matricula;
+		this.nome = nome;
+	}
+
+
+	public Aluno() {
+		super();
+		
+			}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Aluno [id=" + id + ", matricula=" + matricula + ", nome=" + nome + "]";
+	}
+	
+	
+	
+
+}
