76a8e295bf3b2c8a666a9cee946e16094d317eb9
wanessaramos <wanessinha_ramos@yahoo.com.br>
2018-03-14 16:34:54 -0300

    Editando as classes Relatorio e RelatorioTest

--- src/main/java/br/edu/ifal/systemifal/servico/Relatorio.java
+++ src/main/java/br/edu/ifal/systemifal/servico/Relatorio.java
@@ -1,6 +1,10 @@
 package br.edu.ifal.systemifal.servico;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import br.edu.ifal.systemifal.modelo.Nota;
 
@@ -8,17 +12,28 @@ public class Relatorio {
 	
 	private double maiorNota = Double.NEGATIVE_INFINITY;
 	private double menorNota = Double.POSITIVE_INFINITY;
+	private List<Nota> top3Notas = new ArrayList<Nota>();	
 	
 	public void  gerar(List<Nota> notas) {
 		for (Nota nota : notas) {
 			if(nota.getValor() > maiorNota) {
 				maiorNota = nota.getValor();
 			}
-			else if(nota.getValor() < menorNota) {
+			if(nota.getValor() < menorNota) {
 				menorNota = nota.getValor();
 			
 			}
 		}
+		
+		setarMaioresNotas(notas);
+	}
+
+	private void setarMaioresNotas(List<Nota> notas) {
+		List<Nota> notasOrdemDecrescente = notas.stream()
+				.sorted(Comparator.comparing(Nota::getValor))
+				.collect(Collectors.toList());
+		
+		Collections.reverse(notasOrdemDecrescente);
 	}
 
 	public double getMaiorNota() {
--- src/test/java/br/edu/ifal/systemifal/servico/RelatorioTest.java
+++ src/test/java/br/edu/ifal/systemifal/servico/RelatorioTest.java
@@ -1,20 +1,20 @@
 package br.edu.ifal.systemifal.servico;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import br.edu.ifal.systemifal.modelo.Aluno;
 import br.edu.ifal.systemifal.modelo.Disciplina;
 import br.edu.ifal.systemifal.modelo.Nota;
 
+
 public class RelatorioTest {
 
 	@Test
-	public void test() {
+	public void deveFuncionarParaAsNotasNaOrdemAleatoria() {
 		List<Aluno> alunos = new ArrayList<Aluno>();
 		
 		
@@ -38,8 +38,94 @@ public class RelatorioTest {
 		double maiorNotaEsperada = 9.5;
 		double menorNotaEsperada = 2.5;
 		
-		System.out.println(relatorio.getMaiorNota()== maiorNotaEsperada);
-		System.out.println(relatorio.getMenorNota()== menorNotaEsperada);
+		assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
+	}
+	
+	@Test
+	public void deveFuncionarParaAsNotasNaOrdemCrescente() {
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		
+		Aluno ana = new Aluno("20","123456","Ana");
+		Aluno jose = new Aluno("10","12563","José");
+		Aluno tereza = new Aluno("30","158936","Tereza");
+		alunos.add(ana);
+		alunos.add(jose);
+		alunos.add(tereza);
+		
+		Disciplina artes = new Disciplina("2","12556","artes");
+		
+		List<Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1, ana, artes, 2.5));
+		notas.add(new Nota(2, jose, artes, 7.5));
+		notas.add(new Nota(3, tereza, artes, 9.5));
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerar(notas);
+ 
+		double maiorNotaEsperada = 9.5;
+		double menorNotaEsperada = 2.5;
+		
+		assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
+		
+	}
+	
+	@Test
+	public void deveFuncionarParaAsNotasNaOrdemDecrescente() {
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		
+		
+		Aluno ana = new Aluno("20","123456","Ana");
+		Aluno jose = new Aluno("10","12563","José");
+		Aluno tereza = new Aluno("30","158936","Tereza");
+		alunos.add(ana);
+		alunos.add(jose);
+		alunos.add(tereza);
+		
+		Disciplina artes = new Disciplina("2","12556","artes");
+		
+		List<Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1, ana, artes, 9.5));
+		notas.add(new Nota(2, jose, artes, 7.5));
+		notas.add(new Nota(3, tereza, artes, 2.5));
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerar(notas);
+ 
+		double maiorNotaEsperada = 9.5;
+		double menorNotaEsperada = 2.5;
+		
+		assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
+		
+	}
+	
+	@Test
+	public void deveFuncionarParaMaiorEMenorNota() {
+		List<Aluno> aluno = new ArrayList<Aluno>();
+		
+		
+		Aluno ana = new Aluno("20","123456","Ana");
+		aluno.add(ana);
+		
+		
+		Disciplina artes = new Disciplina("2","12556","artes");
+		
+		List<Nota> notas = new ArrayList<Nota>();
+		notas.add(new Nota(1, ana, artes, 9.5 ));
+		
+		
+		Relatorio relatorio = new Relatorio();
+		relatorio.gerar(notas);
+ 
+		double maiorNotaEsperada = 9.5;
+		double menorNotaEsperada = 9.5;
+		
+		assertEquals(maiorNotaEsperada, relatorio.getMaiorNota(), 0000.1);
+		assertEquals(menorNotaEsperada, relatorio.getMenorNota(), 0000.1);
+		
 	}
 
 }
